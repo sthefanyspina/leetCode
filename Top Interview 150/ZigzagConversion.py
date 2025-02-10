ea26d6e005dabe1b623e5aa1052ab6f3bee34d08
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-02-10 06:54:28 -0300

    Create ZigzagConversion.py

---
+++ Top Interview 150/ZigzagConversion.py
@@ -0,0 +1,14 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+        rows = [''] * min(numRows, len(s))
+        curRow, goingDown = 0, False
+
+        for c in s:
+            rows[curRow] += c
+            if curRow == 0 or curRow == numRows - 1:
+                goingDown = not goingDown
+            curRow += 1 if goingDown else -1
+
+        return ''.join(rows)
