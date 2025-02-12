3a3d9db9019b83056b4cc9aa25e1e5480fe9d494
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-02-12 07:01:54 -0300

    Update LongestConsecutiveSequence.py

--- Top Interview 150/LongestConsecutiveSequence.py
+++ Top Interview 150/LongestConsecutiveSequence.py
@@ -1,17 +1,18 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        nums.sort()
-
-        max_streak = 1
-        cur_streak = 1
-
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i-1]:
-                continue
-            elif nums[i] == nums[i-1] + 1:
-                cur_streak += 1
-            else:
-                max_streak = max(max_streak, cur_streak)
-                cur_streak = 1
+        if not nums:
+            return 0
         
-        return  max(max_streak, cur_streak)
+        num_set = set(nums)
+        max_streak = 0
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                cur_streak = 1
+
+                while num + cur_streak in num_set:
+                    cur_streak += 1
+
+                max_streak = max(max_streak, cur_streak)
+
+        return max_streak
