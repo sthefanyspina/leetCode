fa10d1912b13de948d064b4d9f7819f710426e80
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-07-21 08:58:14 -0300

    Update Employees Whose Manager Left the Company.sql

--- SQL50/Employees Whose Manager Left the Company.sql
+++ SQL50/Employees Whose Manager Left the Company.sql
@@ -1,3 +1,3 @@
-SELECT employee_id FROM Employees WHERE salary > 3000
+SELECT employee_id FROM Employees WHERE salary < 30000
 AND manager_id NOT IN (SELECT employee_id FROM Employees)
 ORDER BY employee_id
